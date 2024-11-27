e992ed2d13af1596e16451a35afa9ea66b7c97c0
HomayoonAAhmad <homayoon5600@gmail.com>
2024-11-27 13:08:22 +0330

    future todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import ActionBar from "@/gcui-main/ActionBar";
+import ActionBarDesktop from "@/gcui-main/ActionBarDesktop";
+import Attachment from "@/gcui-main/Attachment";
+import AvatarBox from "@/gcui-main/AvatarBox";
+import AvatarSelector from "@/gcui-main/AvatarSelector";
+import BlogPost from "@/gcui-main/cards/BlogPost";
 import Business from "@/gcui-main/cards/Business";
+import Quote from "@/gcui-main/cards/Quote";
+import Confirm from "@/gcui-main/Confirm";
+import Login from "@/gcui-main/forms/Login";
+import Search from "@/gcui-main/forms/Search";
 import Nav from "@/gcui-main/Nav";
+import Radio from "@/gcui-main/Radio";
+import SearchForm from "@/gcui-main/SearchForm";
 import Service from "@/gcui-main/Service";
+import Share from "@/gcui-main/Share";
 import SideMenu from "@/gcui-main/SideMenu";
 import SideMenuPeer from "@/gcui-main/SideMenuPeer";
 import Ticket from "@/stories/Ticket/Ticket";
@@ -9,16 +23,14 @@ import Ticket from "@/stories/Ticket/Ticket";
 export default function Page() {
   return (
     <div dir="rtl">
-      <Business
-        business={{
-          id: "123",
-          name: "Tech Innovators Inc.",
-
-          website: "https://techinnovators.com",
-          _status: 3, // Under review
-        }}
-        editable={true}
-      />
+      {/* <ActionBarDesktop />
+      <Attachment setFilesInForm={function (files: File[]): void {}} /> */}
+      {/* <BlogPost blog={null} compact={false} /> */}
+      {/* <Quote /> */}
+      {/* <Confirm message={undefined} onConfirm={undefined} children={undefined} /> */}
+      {/* <Share /> */}
+      {/* <Radio name={"undefined"} children={"aausdhvasd"} /> */}
+      {/* <AvatarSelector /> */}
     </div>
   );
 }
